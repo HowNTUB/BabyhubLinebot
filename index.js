0ce9489f2a7a23225ddb3a414461345fd6757153
LIN WAN-HAW <10456003@ntub.edu.tw>
2019-12-11 12:32:22 +0800

    myapp

--- index.js
+++ index.js
@@ -220,13 +220,12 @@ app.post('/dialogflow', express.json(), (req, res) => {
                                 agent.add("👶🏻寶寶資訊");
                                 const lineMessage = {
                                     "type": "image",
-                                    "originalContentUrl": "https://i.imgur.com/GvTlnnj.png",
-                                    "previewImageUrl": "https://i.imgur.com/GvTlnnj.png"
+                                    "originalContentUrl": "https://i.imgur.com/FqSRyzU.png",
+                                    "previewImageUrl": "https://i.imgur.com/FqSRyzU.png"
                                 };
                                 var payload = new Payload('LINE', lineMessage, {
                                     sendAsMessage: true
                                 });
-                                agent.add(payload);
                                 data.forEach(item => {
                                     var emoji;
                                     if (item.gender == "男孩") {
@@ -234,6 +233,7 @@ app.post('/dialogflow', express.json(), (req, res) => {
                                     } else if (item.gender == "女孩") {
                                         emoji = "👧🏻";
                                     }
+                                    agent.add(payload);
                                     if (item.height == null) {
                                         agent.add(item.name + '是個' + emoji + item.gender + '\n🎂生日是' + moment(item.birthday).format("YYYY-MM-DD") + '\n目前還沒有寶寶的身高體重資訊哦😢');
                                     } else {
